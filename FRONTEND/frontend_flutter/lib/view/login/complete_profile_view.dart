3fa697780e3b82b37af8e20fd835e5631f0cc161
SaadSiddiqui134 <saad03siddiqui134@gmail.com>
2024-12-23 14:49:20 +0500

    git test saad

--- FRONTEND/frontend_flutter/lib/view/login/complete_profile_view.dart
+++ FRONTEND/frontend_flutter/lib/view/login/complete_profile_view.dart
@@ -11,6 +11,8 @@ class CompleteProfileView extends StatefulWidget {
   @override
   State<CompleteProfileView> createState() => _CompleteProfileViewState();
 }
+
+//SAAD GIT TEST
 //HASHIR'S CHANGES FOR TESTING
 class _CompleteProfileViewState extends State<CompleteProfileView> {
   TextEditingController txtDate = TextEditingController();
@@ -53,54 +55,54 @@ class _CompleteProfileViewState extends State<CompleteProfileView> {
                   child: Column(
                     children: [
                       Container(
-                          decoration: BoxDecoration(
-                              color: TColor.lightGray,
-                              borderRadius: BorderRadius.circular(15)),
-                          child: Row(
-                            children: [
-                              Container(
-                                  alignment: Alignment.center,
-                                  width: 50,
-                                  height: 50,
-                                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                                  
-                                  child: Image.asset(
-                                    "assets/img/gender.png",
-                                    width: 20,
-                                    height: 20,
-                                    fit: BoxFit.contain,
-                                    color: TColor.gray,
-                                  )),
-                            
-                              Expanded(
-                                child: DropdownButtonHideUnderline(
-                                  child: DropdownButton(
-                                    items: ["Male", "Female"]
-                                        .map((name) => DropdownMenuItem(
-                                              value: name,
-                                              child: Text(
-                                                name,
-                                                style: TextStyle(
-                                                    color: TColor.gray,
-                                                    fontSize: 14),
-                                              ),
-                                            ))
-                                        .toList(),
-                                    onChanged: (value) {},
-                                    isExpanded: true,
-                                    hint: Text(
-                                      "Choose Gender",
-                                      style: TextStyle(
-                                          color: TColor.gray, fontSize: 12),
-                                    ),
+                        decoration: BoxDecoration(
+                            color: TColor.lightGray,
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Row(
+                          children: [
+                            Container(
+                                alignment: Alignment.center,
+                                width: 50,
+                                height: 50,
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 15),
+                                child: Image.asset(
+                                  "assets/img/gender.png",
+                                  width: 20,
+                                  height: 20,
+                                  fit: BoxFit.contain,
+                                  color: TColor.gray,
+                                )),
+                            Expanded(
+                              child: DropdownButtonHideUnderline(
+                                child: DropdownButton(
+                                  items: ["Male", "Female"]
+                                      .map((name) => DropdownMenuItem(
+                                            value: name,
+                                            child: Text(
+                                              name,
+                                              style: TextStyle(
+                                                  color: TColor.gray,
+                                                  fontSize: 14),
+                                            ),
+                                          ))
+                                      .toList(),
+                                  onChanged: (value) {},
+                                  isExpanded: true,
+                                  hint: Text(
+                                    "Choose Gender",
+                                    style: TextStyle(
+                                        color: TColor.gray, fontSize: 12),
                                   ),
                                 ),
                               ),
-
-                             const SizedBox(width: 8,)
-
-                            ],
-                          ),),
+                            ),
+                            const SizedBox(
+                              width: 8,
+                            )
+                          ],
+                        ),
+                      ),
                       SizedBox(
                         height: media.width * 0.04,
                       ),
